fa3242c2e2ab938ea785e0246f4c4cbc3bedd292
mustafjz <must946@gmail.com>
2025-05-15 01:27:27 +0200

    handle server, port, name input

--- client.c
+++ client.c
@@ -10,20 +10,45 @@
 #include <arpa/inet.h>
 #include "cards.c"
 
-#define PORT 25565
 
 int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serv_addr;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);  // localhost
-
-    printf("Was ist dein Name?\n");
+    printf("Server IP:\n");
     printf("-> ");
+
+    char ip[100];
+    fgets(ip, 100, stdin);
+    ip[strcspn(ip, "\n")] = 0;
+    if (ip[0] == '\0') {
+        strcpy(ip, "127.0.0.1");
+    }
+
+    printf("Port:\n");
+    printf("-> ");
+
+    char port[6];
+    fgets(port, 5, stdin);
+    port[strcspn(port, "\n")] = 0;
+
+    if (port[0] == '\0') {
+        strcpy(port, "25565");
+    }
+
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(port));
+    inet_pton(AF_INET, ip, &serv_addr.sin_addr);  // localhost
+    printf("Connected to %s:%s\n", ip, port);
     char name[30];
-    scanf("%s", name);
+    do {
+        printf("Enter your name?\n");
+        printf("-> ");
+        fgets(name, 30, stdin);
+        name[strcspn(name, "\n")] = 0;
+    } while (name[0] == 0);
+
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
         printf("Connection to server has failed!\n");
         exit(1);
@@ -36,8 +61,6 @@ int main() {
 
     read(sock, buffer, sizeof(buffer));
     printf("Server: %s", buffer);
-
-    // game logic
 
 
     close(sock);
