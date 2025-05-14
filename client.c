d701d239f83d3bf11e6dae93aacf3ccd44494678
mustafjz <must946@gmail.com>
2025-05-15 01:48:43 +0200

    add timeout handling

--- client.c
+++ client.c
@@ -8,7 +8,51 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include "cards.c"
+#include "errno.h"
+
+int connect_timeout(int socket, struct sockaddr *address, socklen_t address_length, int timeout) {
+
+    int flags = fcntl(socket, F_GETFL, 0);
+    // non block
+    fcntl(socket, F_SETFL, flags | O_NONBLOCK);
+
+    int result = connect(socket, address, address_length);
+    if (result == 0) {
+        fcntl(socket, F_SETFL, flags | O_NONBLOCK);
+        return 0;
+    } else if (errno != EINPROGRESS) {
+        return -1;
+    }
+
+    fd_set writefds;
+    struct timeval tv;
+
+    FD_ZERO(&writefds);
+    FD_SET(socket, &writefds);
+
+    tv.tv_sec = timeout;
+    tv.tv_usec = 0;
+
+    result = select(socket + 1, NULL, &writefds, NULL, &tv);
+    if (result <= 0) {
+        printf("Connection to server has timed out.\n");
+        return -1;
+    }
+
+    int so_error;
+    socklen_t len = sizeof(so_error);
+    getsockopt(socket, SOL_SOCKET, SO_ERROR, &so_error, &len);
+
+    if (so_error != 0) {
+        printf("Connection to server has failed.\n");
+        return -1;
+    }
+
+    fcntl(socket, F_SETFL, flags & ~O_NONBLOCK);
+    return 0;
+}
 
 
 int main() {
@@ -31,7 +75,6 @@ int main() {
     char port[6];
     fgets(port, 5, stdin);
     port[strcspn(port, "\n")] = 0;
-
     if (port[0] == '\0') {
         strcpy(port, "25565");
     }
@@ -39,7 +82,12 @@ int main() {
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(atoi(port));
-    inet_pton(AF_INET, ip, &serv_addr.sin_addr);  // localhost
+    inet_pton(AF_INET, ip, &serv_addr.sin_addr);
+
+    if ((connect_timeout(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr), 8) == -1)) {
+        exit(1);
+    }
+
     printf("Connected to %s:%s\n", ip, port);
     char name[30];
     do {
@@ -49,10 +97,7 @@ int main() {
         name[strcspn(name, "\n")] = 0;
     } while (name[0] == 0);
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
-        printf("Connection to server has failed!\n");
-        exit(1);
-    }
+
     send(sock, name, strlen(name), 0);
 
     char buffer[1024] = {0};
