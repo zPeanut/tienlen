47e5f4150b5ae5b2b4fa223f31f9a5b753a16a4c
mustafjz <must946@gmail.com>
2025-05-20 00:17:04 +0200

    change shuffle algorithm

--- cards.c
+++ cards.c
@@ -18,10 +18,11 @@ void init_deck(Deck *deck) {
     deck->index = 0;
 }
 
+// fisher yates algorithm
 void shuffle_deck(Deck *deck) {
     srand(time(NULL));
-    for (int i = NUM_CARDS - 1; i > 0; i--) {
-        int j = rand() % i + 1;
+    for (int i = 0; i < NUM_CARDS; i++) {
+        int j = i + (rand() % (53 - i));
         Card temp = deck->cards[i];
         deck->cards[i] = deck->cards[j];
         deck->cards[j] = temp;
